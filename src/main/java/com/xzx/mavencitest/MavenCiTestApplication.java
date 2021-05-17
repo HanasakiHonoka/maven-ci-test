4abc3e82f80ab0ffd6ac9c3ec62fe83c2e9027d2
xuzixiang <xuzixiang@baidu.com>
2021-05-17 10:19:00 +0800

    init

---
+++ src/main/java/com/xzx/mavencitest/MavenCiTestApplication.java
@@ -0,0 +1,13 @@
+package com.xzx.mavencitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenCiTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MavenCiTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xzx/mavencitest/MavenCiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xzx.mavencitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenCiTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
